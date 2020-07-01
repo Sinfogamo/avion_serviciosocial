c69c5c80442337f8cb9556791aef727b5aaba16f
Sinfogamo Gaahl <38165764+Sinfogamo@users.noreply.github.com>
2020-07-01 09:52:17 -0500

    Primer commit, Estructura

---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Juego del Avión',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Juego del Avión'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ButtonTheme(
+                minWidth: 120.0,
+                height: 100.0,
+                  child: RaisedButton(
+                    onPressed: () {  },
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40.0),
+                    ),
+                    child: Text("10", style: TextStyle(color: Colors.white),),
+                  ),
+              ),
+              RaisedButton(
+                onPressed: () {  },
+                color: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+                child: Text("9", style: TextStyle(color: Colors.white),),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () {  },
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40.0),
+                    ),
+                    child: Text("8", style: TextStyle(color: Colors.white),),
+                  ),
+                  Divider(
+                    indent: 10,
+                  ),
+                  RaisedButton(
+                    onPressed: () {  },
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40.0),
+                    ),
+                    child: Text("7", style: TextStyle(color: Colors.white),),
+                  ),
+                ],
+              ),
+              RaisedButton(
+                onPressed: () {  },
+                color: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+                child: Text("6", style: TextStyle(color: Colors.white),),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () {  },
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40.0),
+                    ),
+                    child: Text("5", style: TextStyle(color: Colors.white),),
+                  ),
+                  Divider(
+                    indent: 10,
+                  ),
+                  RaisedButton(
+                    onPressed: () {  },
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40.0),
+                    ),
+                    child: Text("4", style: TextStyle(color: Colors.white),),
+                  ),
+                ],
+              ),
+              RaisedButton(
+                onPressed: () {  },
+                color: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+                child: Text("3", style: TextStyle(color: Colors.white),),
+              ),
+              RaisedButton(
+                onPressed: () {  },
+                color: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+                child: Text("2", style: TextStyle(color: Colors.white),),
+              ),
+              RaisedButton(
+                onPressed: () {  },
+                color: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+                child: Text("1", style: TextStyle(color: Colors.white),),
+              ),
+            ],
+          ),
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
